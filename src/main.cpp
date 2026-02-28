b208b06e1434e29b9bf12526881bbc4ba52a70ab
Daniel Wilk <wilkdaniel03@gmail.com>
2026-02-28 19:35:27 +0100

    Malcomm's precision

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,23 @@ T malcomm_radix() {
 	return b;
 }
 
+template <typename T>
+requires std::floating_point<T>
+T malcomm_precision() {
+	auto radix = malcomm_radix<T>();
+	T a = 1.0f;
+	T i = 0.0f;
+	while((a+1.0f) - a == 1.0f) {
+		a *= radix;
+		i++;
+	}
+	return i;
+}
+
 int main(void) {
-	std::cout << "Radix: " << malcomm_radix<float>() << std::endl;
-	std::cout << "Radix: " << malcomm_radix<double>() << std::endl;
+	std::cout << "Radix(32-bit): " << malcomm_radix<float>() << std::endl;
+	std::cout << "Radix(64-bit): " << malcomm_radix<double>() << std::endl;
+	std::cout << "Precision(32-bit): " << malcomm_precision<float>() << std::endl;
+	std::cout << "Precision(64-bit): " << malcomm_precision<double>() << std::endl;
 	return EXIT_SUCCESS;
 }
