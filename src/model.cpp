d4e7d965b107a9466392bf6d10a79b3a1d8ada05
Daniel Wilk <wilkdaniel03@gmail.com>
2026-03-01 13:56:09 +0100

    [c++ model] counter implementation

--- src/model.cpp
+++ src/model.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstdint>
+#include <concepts>
+#include <cmath>
 
 #include <ac_int.h>
 
 template <uint8_t size>
+concept SizeConcept = size <= 64;
+
+template <uint8_t size>
+requires SizeConcept<size>
 class ShiftRegister {
 	ac_int<size,false> reg;
 	bool directionRight;
 public:
 	ShiftRegister() : reg(ac_int<size,false>(0)), directionRight(true) {}
-	ShiftRegister(const uint64_t val) : reg(ac_int<size,false>(val)) {}
+	ShiftRegister(const int32_t val) : reg(ac_int<size,false>(val)) {}
 	ShiftRegister(const bool directionRight) : reg(ac_int<size,false>(0)), directionRight(directionRight) {}
 	ShiftRegister(const uint64_t val, const bool directionRight) : reg(ac_int<size,false>(val)), directionRight(directionRight) {}
 	uint64_t operator*() const {
-		return this->reg.to_int64();
+		return this->reg.to_uint64();
 	}
 	void Shift() {
 		if(this->directionRight) {
@@ -25,16 +31,28 @@ public:
 	}
 };
 
+template <uint8_t size>
+requires SizeConcept<size>
+class Counter {
+	ac_int<size,false> reg;
+public:
+	Counter() : reg(ac_int<size,false>(0)) {}
+	Counter(const int32_t val) : reg(ac_int<size,false>(val)) {}
+	uint64_t operator*() const {
+		return this->reg.to_uint64();
+	}
+	void operator+=(const uint32_t val) {
+		this->reg += val;
+	}
+	bool CheckRco() const {
+		auto maxValue = std::pow(2.0,size) - 1.0;
+		return this->reg.to_uint64() == maxValue;
+	}
+};
+
 int main(void) {
-	ShiftRegister<10> reg(13,false);
-	std::cout << *reg << std::endl;
-	reg.Shift();
-	std::cout << *reg << std::endl;
-	reg.Shift();
-	std::cout << *reg << std::endl;
-	reg.Shift();
-	std::cout << *reg << std::endl;
-	reg.Shift();
+	ShiftRegister<8> reg(13);
+	Counter<2> counter;
 
 	return EXIT_SUCCESS;
 }
