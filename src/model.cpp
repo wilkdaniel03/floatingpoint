f4802046329c9554a0fd23ed633462199c444d61
Daniel Wilk <wilkdaniel03@gmail.com>
2026-03-01 15:20:06 +0100

    [c++ model] N bit shifter implementation

--- src/model.cpp
+++ src/model.cpp
@@ -3,6 +3,8 @@
 #include <cstdint>
 #include <concepts>
 #include <cmath>
+#include <memory>
+#include <any>
 
 #include <ac_int.h>
 
@@ -16,7 +18,7 @@ class ShiftRegister {
 	bool directionRight;
 public:
 	ShiftRegister() : reg(ac_int<size,false>(0)), directionRight(true) {}
-	ShiftRegister(const int32_t val) : reg(ac_int<size,false>(val)) {}
+	ShiftRegister(const int32_t val) : reg(ac_int<size,false>(val)), directionRight(true) {}
 	ShiftRegister(const bool directionRight) : reg(ac_int<size,false>(0)), directionRight(directionRight) {}
 	ShiftRegister(const uint64_t val, const bool directionRight) : reg(ac_int<size,false>(val)), directionRight(directionRight) {}
 	uint64_t operator*() const {
@@ -44,15 +46,43 @@ public:
 	void operator+=(const uint32_t val) {
 		this->reg += val;
 	}
+	void operator-=(const uint32_t val) {
+		this->reg -= val;
+	}
 	bool CheckRco() const {
 		auto maxValue = std::pow(2.0,size) - 1.0;
 		return this->reg.to_uint64() == maxValue;
 	}
+	bool CheckEmpty() const {
+		return this->reg.to_uint64() == 0;
+	}
+};
+
+template <uint8_t size>
+requires SizeConcept<size>
+class NBitShifter {
+	static constexpr uint8_t counterWidth = std::ceil(std::log2(size))+1;
+	ShiftRegister<size> reg;
+	Counter<counterWidth> counter;
+public:
+	NBitShifter() : counter(Counter<counterWidth>((int32_t) std::pow(2.0,counterWidth))) {}
+	NBitShifter(const int32_t val, const int32_t shiftCount) : reg(ShiftRegister<size>(val)), counter(Counter<counterWidth>(shiftCount)) {}
+	void Shift() {
+		while(*this->counter != 0) {
+			this->reg.Shift();
+			this->counter -= 1;
+		}
+	}
+	uint64_t operator*() const {
+		return *this->reg;
+	}
 };
 
 int main(void) {
-	ShiftRegister<8> reg(13);
-	Counter<2> counter;
+	NBitShifter<10> shifter(327,6);
+	std::cout << "Start -> " << *shifter << std::endl;
+	shifter.Shift();
+	std::cout << *shifter << ", " << (327 >> 6) << std::endl;
 
 	return EXIT_SUCCESS;
 }
